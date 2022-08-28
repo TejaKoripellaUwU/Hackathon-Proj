ac0978557e9e3658dd85ee827868a4eaf3dde979
Teja Koripella <s-tkoripella@lwsd.org>
2022-08-28 03:31:30 -0700

    Update Customize.js

--- Website/static/Customize.js
+++ Website/static/Customize.js
@@ -1,6 +1,11 @@
-let select = document.querySelector("select")
-let tGetter = document.querySelector("#getTopics")
-let add = document.querySelector("#containerAdd")
+let select = document.querySelector("select");
+let tGetter = document.querySelector("#getTopics");
+let remove = document.querySelector("#containerRemove");
+let removeTopic = document.querySelector("#remove");
+let add = document.querySelector("#containerAdd");
+let addTopic = document.querySelector("#add");
+let finalFunctions = [];
+
 tGetter.addEventListener("click",()=>{
     add.replaceChildren();
     output = select.value;
@@ -11,17 +16,33 @@ tGetter.addEventListener("click",()=>{
             grade:output
         },
         success: function (response) {
+            let labels = []
             for (item of response){
                 let check = document.createElement('input')
                 check.type = "checkbox"
                 check.name = item[1];
+                check.raw = item[0]
                 add.appendChild(check);
                 let label = document.createElement("label")
                 label.for = item[1];
                 add.appendChild(label).innerHTML ='<label>'+item[0]+'</label>'
+                labels.push(label)
             }
+            addTopic.addEventListener("click",()=>{
+                for (child of add.children){
+                    if(child.checked){
+                        finalFunctions.push(child.textContent)
+                        remove.appendChild(child)
+                        let label = document.createElement("label")
+                        label.for = child.name;
+                        remove.appendChild(label).innerHTML ='<label>'+child.raw+'</label>'
+                        
+                    }
+                }
+            })
         },
         error: function (response) {
         }
     });
 })
+
